f7149d20e1916f5e869f791498f110911b3891e8
Sudzzz <sudeshnasarkar4421@gmail.com>
2025-10-09 19:56:51 +0530

    Add files via upload

---
+++ mail.py
@@ -0,0 +1,5 @@
+MODEL_PATH = r"C:\safetyeye\runs\helmet_augmented_final\yolo_augmented_m_fresh\weights\best.pt"  # Your YOLO model path
+EMAIL_ADDRESS = "minimeeee44@gmail.com"      # Your Gmail
+EMAIL_PASSWORD = "nptn heyq glcp iltg"   # App password
+TO_EMAIL = "minimeeee44@gmail.com"      # Recipient email
+CAMERA_INDEX = 0                             # Change if needed
